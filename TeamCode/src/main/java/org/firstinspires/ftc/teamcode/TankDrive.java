c5d11cfd4bf5c94160bddd8ec651c4279d655f33
Hanson C. <47832494+Hanson2258@users.noreply.github.com>
2025-11-29 07:52:06 -0800

    Updated TankDrive.java program to meet specifications
    - Corrected Variable name
    - Changed constants
    - Added PIDF for shooter
    - Fixed Driving
    - Changed calls to shooters and servos to only be called when needed instead of every loop
    - Added telem for actual shooter RPM
    - Fixed slow speed mode
    - Fixed range clipping for fast mode
    - Created function for setting shooter RPM

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
@@ -20,13 +20,13 @@ public class TankDrive extends OpMode {
     private CRServo crServo = null;
 
     //// Constants Declaration
-    private final double MAX_SLOW_SPEED     = 0.5;
+    private final double SLOW_SPEED_DIVIDER = 0.5;
     private final double STARTING_SERVO_POS = 0.5;
 
     // Shooter
-    private final double SHOOTER_TICKS_PER_SEC = 28 * 4; // GOBILDA 5203-2402-0001 PPR is 28
+    private final double SHOOTER_TICKS_PER_REV = 28 * 2; // GOBILDA 5203-2402-0001 PPR is 28
     private final double SHOOTER_STOPPED       =    0;
-    private final double MIN_SHOOTER_RPM       = 1500;
+    private final double MIN_SHOOTER_RPM       =  500;
     private final double MID_SHOOTER_RPM       = 3000;
     private final double MAX_SHOOTER_RPM       = 6000;
     private final double SHOOTER_INC_AND_DEC   =    5;
@@ -35,13 +35,13 @@ public class TankDrive extends OpMode {
     private final double SERVO_START_POS   = 0.0;
     private final double SERVO_MID_POS     = 0.5;
     private final double SERVO_END_POS     = 1.0;
-    private final double SERVO_INC_AND_DEC = 0.01;
+    private final double SERVO_INC_AND_DEC = 0.001;
 
     // Servo Speed
     private final double CRSERVO_REVERSE_SPEED = -1.0;
     private final double CRSERVO_STOP_SPEED    =  0.0;
     private final double CRSERVO_FORWARD_SPEED =  1.0;
-    private final double CRSERVO_INC_AND_DEC   =  0.05;
+    private final double CRSERVO_INC_AND_DEC   =  0.001;
 
     //// Variables
     private boolean fastModeEnabled   = false;
@@ -72,6 +72,9 @@ public class TankDrive extends OpMode {
         rightDrive.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
         shooter.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
 
+        // Shooter PIDF
+        shooter.setVelocityPIDFCoefficients(10.0, 0.0, 0.0, 0.0);
+
         // Define and initialize ALL installed servos.
         servo   = hardwareMap.get(Servo.class, "servo");
         crServo = hardwareMap.get(CRServo.class, "CRServo");
@@ -88,47 +91,57 @@ public class TankDrive extends OpMode {
 
         // Drive Input (duplicated for Gamepad 1 and 2)
         if (!gamepad1.atRest()) {
-            arcadeDrive(-gamepad1.right_stick_y, gamepad1.right_stick_x);
+            arcadeDrive(-gamepad1.right_stick_y, -gamepad1.right_stick_x);
         }
         else {
-            arcadeDrive(-gamepad2.right_stick_y, gamepad2.right_stick_x);
+            arcadeDrive(-gamepad2.right_stick_y, -gamepad2.right_stick_x);
         }
 
         /// Gamepad 1 - Specific values
         // Motor to set velocity
         if (gamepad1.dpad_up) {
             targetShooterRPM = MAX_SHOOTER_RPM;
+            setShooterRPM(targetShooterRPM);
         }
         else if (gamepad1.dpad_right) {
             targetShooterRPM = MID_SHOOTER_RPM;
+            setShooterRPM(targetShooterRPM);
         }
         else if (gamepad1.dpad_down) {
             targetShooterRPM = MIN_SHOOTER_RPM;
+            setShooterRPM(targetShooterRPM);
         }
         else if (gamepad1.dpad_left) {
             targetShooterRPM = SHOOTER_STOPPED;
+            setShooterRPM(targetShooterRPM);
         }
 
         // Servo Pos Control
         if (gamepad1.x) {
             targetServoPos = SERVO_START_POS;
+            servo.setPosition(targetServoPos);
         }
         else if (gamepad1.y) {
             targetServoPos = SERVO_MID_POS;
+            servo.setPosition(targetServoPos);
         }
         else if (gamepad1.b) {
             targetServoPos = SERVO_END_POS;
+            servo.setPosition(targetServoPos);
         }
 
         // Servo Speed Control
         if (gamepad1.left_trigger > 0.5) {
             targetCRServoSpeed = CRSERVO_FORWARD_SPEED;
+            crServo.setPower(targetCRServoSpeed);
         }
         else if (gamepad1.right_trigger > 0.5) {
             targetCRServoSpeed = CRSERVO_REVERSE_SPEED;
+            crServo.setPower(targetCRServoSpeed);
         }
         else if (gamepad1.left_bumper) {
             targetCRServoSpeed = CRSERVO_STOP_SPEED;
+            crServo.setPower(targetCRServoSpeed);
         }
 
         /// Gamepad 2 - Variable values
@@ -136,59 +149,64 @@ public class TankDrive extends OpMode {
         if (gamepad2.dpad_up) {
             targetShooterRPM += SHOOTER_INC_AND_DEC;
             targetShooterRPM = Range.clip(targetShooterRPM, MIN_SHOOTER_RPM, MAX_SHOOTER_RPM);
+            setShooterRPM(targetShooterRPM);
         }
         else if (gamepad2.dpad_right) {
             targetShooterRPM = MID_SHOOTER_RPM;
+            setShooterRPM(targetShooterRPM);
         }
         else if (gamepad2.dpad_down) {
             targetShooterRPM -= SHOOTER_INC_AND_DEC;
             targetShooterRPM = Range.clip(targetShooterRPM, MIN_SHOOTER_RPM, MAX_SHOOTER_RPM);
+            setShooterRPM(targetShooterRPM);
         }
         else if (gamepad2.dpad_left) {
             targetShooterRPM = SHOOTER_STOPPED;
+            setShooterRPM(targetShooterRPM);
+
         }
 
         // Servo Pos Control
         if (gamepad2.x) {
             targetServoPos -= SERVO_INC_AND_DEC;
             targetServoPos = Range.clip(targetServoPos, SERVO_START_POS, SERVO_END_POS);
+            servo.setPosition(targetServoPos);
         }
         else if (gamepad2.y) {
             targetServoPos = SERVO_MID_POS;
+            servo.setPosition(targetServoPos);
         }
         else if (gamepad2.b) {
             targetServoPos += SERVO_INC_AND_DEC;
             targetServoPos = Range.clip(targetServoPos, SERVO_START_POS, SERVO_END_POS);
+            servo.setPosition(targetServoPos);
         }
 
         // Servo Speed Control
         if (gamepad2.left_trigger > 0.5) {
             targetCRServoSpeed -= CRSERVO_INC_AND_DEC;
             targetCRServoSpeed = Range.clip(targetCRServoSpeed, CRSERVO_REVERSE_SPEED, CRSERVO_FORWARD_SPEED);
+            crServo.setPower(targetCRServoSpeed);
         }
         else if (gamepad2.right_trigger > 0.5) {
             targetCRServoSpeed += CRSERVO_INC_AND_DEC;
             targetCRServoSpeed = Range.clip(targetCRServoSpeed, CRSERVO_REVERSE_SPEED, CRSERVO_FORWARD_SPEED);
+            crServo.setPower(targetCRServoSpeed);
         }
         else if (gamepad2.left_bumper) {
             targetCRServoSpeed = CRSERVO_STOP_SPEED;
+            crServo.setPower(targetCRServoSpeed);
         }
-        
+
+
         /// Telemetry Update
         telemetry.addData("Drive Left Power", leftDrive.getPower());
         telemetry.addData("Drive Right Power", rightDrive.getPower());
         telemetry.addData("Target Shooter RPM", targetShooterRPM);
+        telemetry.addData("Actual Shooter RPM", shooter.getVelocity() * 2);
         telemetry.addData("Target Servo Pos", targetServoPos);
         telemetry.addData("Target CR Servo Speed", targetCRServoSpeed);
         telemetry.update();
-
-
-        /// Setting Motor and Servo positions/speeds
-        double targetTicksPerSec = (targetShooterRPM / 60.0) * SHOOTER_TICKS_PER_SEC;
-        shooter.setVelocity(targetTicksPerSec);
-        servo.setPosition(targetServoPos);
-        crServo.setPower(targetCRServoSpeed);
-
     }
 
     /**
@@ -209,16 +227,25 @@ public class TankDrive extends OpMode {
         // Clip the values to the range [-MAX_SLOW_SPEED, MAX_SLOW_SPEED] or [-1, 1], depending
         // on if fast mode is enabled or not.
         if (!fastModeEnabled) {
-            leftPower  = Math.max(-MAX_SLOW_SPEED, Math.min(MAX_SLOW_SPEED, leftPower));
-            rightPower = Math.max(-MAX_SLOW_SPEED, Math.min(MAX_SLOW_SPEED, rightPower));
+            leftPower  *= SLOW_SPEED_DIVIDER;
+            rightPower *= SLOW_SPEED_DIVIDER;
         }
         else {
-            leftPower  = Math.max(-1.0, Math.min(1.0, leftPower));
-            rightPower = Math.max(-1.0, Math.min(1.0, rightPower));
+            leftPower  = Range.clip(leftPower, -1.0, 1.0);
+            rightPower  = Range.clip(rightPower, -1.0, 1.0);
         }
 
         // Send power to motors
         leftDrive.setPower(leftPower);
         rightDrive.setPower(rightPower);
+    }
+
+    /**
+     * Calculate the shooter RPM and use built in setVelocity from DcMotorEx.
+     * @param targetRPM for the shooter
+     */
+    private void setShooterRPM(double targetRPM) {
+        double targetTicksPerSec = (targetRPM / 60.0) * SHOOTER_TICKS_PER_REV;
+        shooter.setVelocity(targetTicksPerSec);
     }
 }
